7878c741d757b74af1d242124b163ac7c8dad0a8
ektoras <komposektoras@gmail.com>
2023-03-30 12:15:25 +0300

    train1

---
+++ js-files/train1.js
@@ -0,0 +1,18 @@
+console.log("helldo");
+
+console.log("helldo");
+
+//window.alert("ooops you are bad!");
+
+let x = 10;
+let name = "ektoras";
+let i = false;
+
+x = x + 1;
+name = name + 1
+
+console.log('jdj',x);
+console.log("hi",name);
+console.log(i, "oksi");
+
+document.getElementById("p1").innerHTML = "hello" + name;
